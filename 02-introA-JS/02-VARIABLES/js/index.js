221ef61310f08d4687f652941ec35bfed478183c
Hector Salvador Lopez Jimenez <hector.salvador.lopez.jimenez@gmail.com>
2022-01-26 16:44:04 -0600

    SI

---
+++ 02-introA-JS/02-VARIABLES/js/index.js
@@ -0,0 +1,14 @@
+//datos de entrada que necesito //
+var nombre;
+var apellido;
+var añoNacimiento;
+var añoActual;
+nombre=prompt("ingresa tu nombre");
+apellido=prompt("ingresa tu apellido");
+añoNacimiento=prompt("ingresa  tu año de nacimiento");
+añoActual=prompt("in gresa tu año actual");
+
+// que tengo que hacer con esos datos //
+edad = añoActual - añoNacimiento;
+// como y que tengo que mostrar al usuario //
+document.write("hola + nombre + apellido + tu edad es;)
